5fd0854201871b773cc12346eacfc7bb1c0b268e
Li Kai <falconets25601@gmail.com>
2016-08-12 12:02:46 +0800

    changed routes for server

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const db = require('../queries');
 
-router.get('/api/modules/:school/:year/:sem/:code', db.getSingleModule);
-router.get('/api/modulesList/:school/:year/:sem', db.getModulesList);
+router.get('/modules/:school/:year/:sem/:code', db.getSingleModule);
+router.get('/modulesList/:school/:year/:sem', db.getModulesList);
 
 module.exports = router;
