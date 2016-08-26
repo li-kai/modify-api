fdb437b982d4a26185ccb1e6c9aada93b3f65114
Kai <falconets25601@gmail.com>
2016-08-26 03:02:43 +0000

    Add authentication end point

--- auth/passport.js
+++ auth/passport.js
@@ -84,7 +84,7 @@ module.exports = function(passport) {
   // =========================================================================
   // we are using named strategies since we have one for login and one for signup
   // by default, if there was no name, it would just be called 'local'
-
+  /*
   passport.use('local-login', new LocalStrategy({
     // by default, local strategy uses username and password, we will override with email
     usernameField : 'email',
@@ -110,4 +110,5 @@ module.exports = function(passport) {
     });
     
   }));
+  */
 };
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 const db = require('../queries');
 
 router.get('/modules/:school/:year/:sem/:code', db.getSingleModule);
 router.get('/modulesList/:school/:year/:sem', db.getModulesList);
+router.get('/users', db.getUsersList);
+router.post('/signup', passport.authenticate('local-signup', {
+        successRedirect : '/profile', // redirect to the secure profile section
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    }));
 
 module.exports = router;
